39cc3d270cb14893313aa4dcfddb2bc06c5d00e9
osayijoy <osayijoy16@gmail.com>
2022-12-30 13:13:50 +0100

    first commit

--- src/main/java/hashTable/Main.java
+++ src/main/java/hashTable/Main.java
@@ -1,7 +1,16 @@
-package org.example;
+package hashTable;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        DirectChaining directChaining = new DirectChaining(15);
+        directChaining.insertHashTable("The");
+        directChaining.insertHashTable("Quick");
+        directChaining.insertHashTable("Brown");
+        directChaining.insertHashTable("Fox");
+        directChaining.insertHashTable("Over");
+        directChaining.displayHashTable();
+        directChaining.searchHashTable("Over");
+        directChaining.deleteKeyHashTable("Fox");
+
     }
 }
